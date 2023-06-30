f60e2b535fe9574e80beb3aae2918badc8778dc1
Zoe0426 <90545407+Zoe0426@users.noreply.github.com>
2023-06-30 17:48:11 +0800

    first

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,5 @@ export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page)
   
-
   return getLayout(<Component {...pageProps} />)
 }
