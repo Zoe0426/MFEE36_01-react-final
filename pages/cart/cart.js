bf26aed7b33d8ac6b8860c0715448c4afe073424
Lily Kuo <ilwitulily0209@gmail.com>
2023-06-30 18:22:42 +0800

    cart

---
+++ pages/cart/cart.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DefaultLayout from '@/components/layout/default-layout'
+
+export default function Cart() {
+  return (
+    <>
+      <div className="container-outer">
+        <div className="container-inner">Hello</div>
+      </div>
+    </>
+  )
+}
