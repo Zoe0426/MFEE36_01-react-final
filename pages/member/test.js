28ade2fe455a46a5eb316901f3923ed5a43a6a16
Zoe0426 <90545407+Zoe0426@users.noreply.github.com>
2023-07-01 13:00:24 +0800

    laouyout fixed

---
+++ pages/member/test.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import DefaultLayout from '@/components/layout/default-layout';
+
+export default function Test() {
+  return (
+    <>
+      <DefaultLayout>
+        <div className="container-inner">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          voluptatibus quia, doloremque facere nesciunt dolore corrupti ex nisi
+          quis eligendi optio alias itaque odio voluptates voluptas cupiditate
+          et? Laudantium omnis aspernatur nisi? Blanditiis adipisci cupiditate,
+          eum ex minima repellendus quidem consectetur animi laborum odio quae
+          illo repellat ducimus mollitia excepturi architecto, autem, voluptas
+          explicabo nisi? Dolores quibusdam, nesciunt delectus pariatur
+          voluptatem doloremque eligendi suscipit cupiditate recusandae labore
+          odio officiis dolor sint dolore a cumque quo saepe nostrum excepturi.
+          Debitis, vero minima? Ducimus iste voluptas accusamus est, neque
+          inventore, sequi pariatur recusandae hic numquam dolor, nam doloribus
+          officiis nobis. Modi quo iusto, fugit cum soluta non natus id amet
+          ratione impedit exercitationem laborum distinctio quos porro totam
+          dicta maiores error tempora deserunt ducimus animi atque nostrum iure.
+          Sed nulla totam at distinctio consequuntur, eveniet quas aspernatur
+          ipsam cumque rem tempora odio accusamus, quae fugit! Ea doloribus ut
+          libero accusamus minus adipisci, eum quisquam optio minima, quo hic
+          ratione debitis? Delectus eligendi officiis, repudiandae sed commodi
+          cum aspernatur esse dolor voluptatum doloribus harum, animi vel
+          laboriosam enim, fugiat culpa sapiente optio fuga? Molestiae,
+          inventore velit aut, nostrum expedita, natus id deserunt consequatur
+          debitis voluptates reiciendis molestias autem iusto vero. Incidunt
+          ipsam ducimus nihil cupiditate dolor officiis quidem voluptate
+          necessitatibus, nesciunt optio beatae autem totam animi voluptas
+          neque! Earum vitae, quis sint, natus temporibus explicabo officia
+          eligendi doloribus obcaecati molestiae numquam rerum id! Autem non
+          recusandae, eum incidunt reiciendis repellat soluta dolores sed atque
+          eveniet numquam. Quos doloribus dolorum, deserunt fugiat dolor quam
+          cum odio vero molestiae in explicabo ad alias nihil eos perferendis
+          ipsum? Sapiente aliquid reiciendis sit? Error vel fugit animi sed,
+          minus inventore itaque, necessitatibus distinctio et recusandae
+          dolores ut a vitae fugiat cumque, quasi repudiandae ad officia dicta?
+          Earum fuga libero ut quaerat tempora hic cumque tempore quas!
+          Inventore voluptatum cumque tenetur vero odio laudantium, cum placeat
+          atque eveniet aliquam ipsam quis ab veritatis temporibus magnam
+          adipisci blanditiis hic perferendis dolore iste dolorem impedit optio.
+          Tempore velit non dolor atque blanditiis, quisquam amet, laudantium
+          ratione fugiat excepturi, architecto cupiditate consequuntur explicabo
+          facilis corrupti numquam! Accusamus deserunt sint harum
+          exercitationem! Voluptatem, eligendi incidunt, beatae ipsa sed ducimus
+          nostrum corrupti impedit veritatis distinctio, eaque eveniet quia
+          explicabo enim commodi itaque velit voluptatibus nihil unde quo
+          deserunt aut maiores. Inventore fugit dignissimos consectetur
+          distinctio necessitatibus dolor quisquam quidem, dolores perspiciatis
+          omnis optio ad, iusto officiis! Ipsam repellendus deserunt nesciunt
+          maxime nobis voluptatum! Dignissimos, natus voluptatibus praesentium
+          sit voluptatum, unde temporibus obcaecati provident amet dolorem
+          asperiores est aliquam ut corrupti! Adipisci aperiam autem quaerat
+          accusamus repudiandae. Sequi ex recusandae, quas laborum, rerum minus
+          quos est exercitationem odit rem iure nam accusantium ab? Praesentium,
+          eos ipsam accusantium quasi autem sequi facere eius dolores voluptates
+          et ipsa maxime porro, magni maiores. Vel ratione aliquid provident
+          facilis minima iste labore suscipit quas rerum similique minus ut
+          excepturi cumque, culpa quam voluptatem. Corporis amet molestias nemo,
+          nam nostrum quod debitis ipsum repudiandae facere modi architecto
+          itaque molestiae sed hic voluptates odio ducimus, vitae, praesentium
+          reprehenderit enim in.
+        </div>
+      </DefaultLayout>
+    </>
+  );
+}
