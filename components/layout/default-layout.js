d4bd567f8b86124717338724b3e5c25e43f851e9
Shuyi <shuyi041@gmail.com>
2023-07-03 16:59:54 +0800

    s_update_as main

--- components/layout/default-layout.js
+++ components/layout/default-layout.js
@@ -1,22 +1,14 @@
-import Navbar from './navbar';
-import Footer from './footer';
-import Styles from './navbar.module.css';
+import Navbar from './navbar'
+import Footer from './footer'
 
 export default function DefaultLayout({ children }) {
   return (
     <>
       <div className="container-outer">
         <Navbar />
-        <div className="container-inner">
-          <div className={Styles.content}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
-            inventore quam sit sunt, aspernatur possimus, officiis unde esse,
-            ipsa hic amet labore ea architecto dolorem consequatur. Nihil atque
-            architecto provident ea unde quisquam nulla cumque mollitia beatae
-          </div>
-        </div>
+        <main className="main">{children}</main>
         <Footer />
       </div>
     </>
-  );
+  )
 }
