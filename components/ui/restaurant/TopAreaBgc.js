c5baac469a3b784aa8c18518856d84d1b647d303
jill <jillwunnie1213@gmail.com>
2023-07-04 17:21:34 +0800

    restuarnt

---
+++ components/ui/restaurant/TopAreaBgc.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import topAreaBgc from '@/assets/top_area_bgc.svg'
+import cat from '@/assets/cat.svg'
+import tree from '@/assets/tree.svg'
+
+export default function TopAreaBgc() {
+  return (
+    <>
+      <Image src={topAreaBgc} />
+      <Image src={cat} />
+      <Image src={tree} />
+    </>
+  )
+}
