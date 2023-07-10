532de43e83ef6d5d3a3851b25c5fd87659d91b32
Zoe0426 <90545407+Zoe0426@users.noreply.github.com>
2023-07-10 16:58:28 +0800

    會員表單

--- components/ui/forms/SignInForm.js
+++ components/ui/forms/SignInForm.js
@@ -1,17 +1,71 @@
 import React from 'react';
 import Styles from './SignInForm.module.css';
+import MainBtn from '../buttons/MainBtn';
+import SecondaryBtn from '../buttons/SecondaryBtn';
+import Link from 'next/link';
+import { Form, Input } from 'antd';
 
 export default function SignInForm() {
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
     <>
-      <div className={Styles.form}></div>
-      <div className={Styles.btns}></div>
-      <div className={Styles.line}>
-        <div className={Styles.lineText}>更多登入方式</div>
-      </div>
-      <div className={Styles.google}>
-        <img src="/member-center-images/google.svg" alt="" />
-      </div>
+      <Form
+        name="loginForm"
+        className={Styles.loginForm}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          className={Styles.formItem}
+          name={'email'}
+          rules={[
+            {
+              required: true,
+              type: 'email',
+              message: '請輸入正確Email',
+            },
+          ]}
+        >
+          <Input placeholder="請輸入Email" size="large" />
+        </Form.Item>
+        <Form.Item
+          className={Styles.formItem}
+          name={'password'}
+          rules={[
+            {
+              required: true,
+              message: '請輸入密碼',
+            },
+          ]}
+        >
+          <Input.Password placeholder="請輸入密碼" size="large" />
+        </Form.Item>
+        <div className={Styles.btnsContain}>
+          <Link href="/shop" className={Styles.forgetPwd}>
+            忘記密碼？
+          </Link>
+          <div className={Styles.btns}>
+            <Link href="/shop" className={Styles.btn}>
+              <SecondaryBtn text="註冊" />
+            </Link>
+            <div className={Styles.btn}>
+              <MainBtn text="登入" />
+            </div>
+          </div>
+        </div>
+        <div className={Styles.line}>
+          <div className={Styles.lineText}>更多登入方式</div>
+        </div>
+        <div className={Styles.google}>
+          <img src="/member-center-images/google.svg" alt="" />
+        </div>
+      </Form>
     </>
   );
 }
--- components/ui/forms/SignUpForm.js
+++ components/ui/forms/SignUpForm.js
@@ -1,11 +1,161 @@
 import React from 'react';
 import Styles from './SignUpForm.module.css';
+import Link from 'next/link';
+import SecondaryBtn from '../buttons/SecondaryBtn';
+import MainBtn from '../buttons/MainBtn';
+import { Form, Input, Radio, Select, Row, Col, ConfigProvider } from 'antd';
 
 export default function SignUpForm() {
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
   return (
     <>
-      <div className={Styles.signUpForm}></div>
-      <div className={Styles.btns}></div>
+      <Form
+        name="signUpForm"
+        className={Styles.signUpForm}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Row gutter={16}>
+          <Col xl={12} lg={24} md={24} sm={24}>
+            <Form.Item
+              label="姓名"
+              className={Styles.formItem}
+              name={'name'}
+              rules={[
+                {
+                  required: true,
+                  message: '請輸入姓名',
+                },
+              ]}
+            >
+              <Input size="large" />
+            </Form.Item>
+          </Col>
+          <Col xl={12} lg={24} md={24} sm={24}>
+            <Form.Item
+              label="手機"
+              className={Styles.formItem}
+              name={'mobile'}
+              rules={[
+                {
+                  required: true,
+                  message: '請輸入手機',
+                },
+              ]}
+            >
+              <Input size="large" />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Form.Item
+          label="帳號"
+          className={Styles.formItem}
+          name={'email'}
+          rules={[
+            {
+              required: true,
+              type: 'email',
+              message: '請輸入正確Email',
+            },
+          ]}
+        >
+          <Input placeholder="請輸入Email" size="large" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          label="密碼"
+          rules={[
+            {
+              required: true,
+              message: '請輸入密碼',
+            },
+          ]}
+          hasFeedback
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="confirm"
+          label="確認密碼"
+          dependencies={['password']}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: '請輸入密碼',
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue('password') === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(new Error('密碼不相符，請重新輸入'));
+              },
+            }),
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item name="gender" label="性別">
+          <ConfigProvider
+            theme={{
+              components: {
+                Radio: {
+                  colorPrimary: '#FD8C46',
+                },
+              },
+            }}
+          >
+            <Radio.Group>
+              <Radio value="男"> 男 </Radio>
+              <Radio value="女"> 女 </Radio>
+              <Radio value="其他"> 其他 </Radio>
+            </Radio.Group>
+          </ConfigProvider>
+        </Form.Item>
+        <Form.Item name="pet" label="寵物">
+          <Radio.Group>
+            <Radio value="狗"> 狗 </Radio>
+            <Radio value="貓"> 貓 </Radio>
+            <Radio value="狗貓"> 狗貓 </Radio>
+            <Radio value="其他"> 其他 </Radio>
+          </Radio.Group>
+        </Form.Item>
+        <Row gutter={16}>
+          <Col xl={12} lg={24} md={24} sm={24} xs={24}>
+            <Form.Item name="city" label="縣市">
+              <Select>
+                <Select.Option value="demo">Demo</Select.Option>
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col xl={12} lg={24} md={24} sm={24} xs={24}>
+            <Form.Item name="area" label="鄉/鎮/區">
+              <Select>
+                <Select.Option value="demo">Demo</Select.Option>
+              </Select>
+            </Form.Item>
+          </Col>
+        </Row>
+        <Form.Item label="地址" className={Styles.formItem} name={'address'}>
+          <Input size="large" />
+        </Form.Item>
+
+        <div className={Styles.btns}>
+          <Link href="/shop" className={Styles.btn}>
+            <SecondaryBtn text="取消" />
+          </Link>
+          <div className={Styles.btn}>
+            <MainBtn text="完成" />
+          </div>
+        </div>
+      </Form>
     </>
   );
 }
