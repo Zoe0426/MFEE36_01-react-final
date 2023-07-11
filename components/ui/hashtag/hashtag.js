6ca82dd5e9081dc50dad06adcadfe1432680cea2
Lily Kuo <ilwitulily0209@gmail.com>
2023-07-11 08:07:45 +0800

    complete

--- components/ui/hashtag/hashtag.js
+++ components/ui/hashtag/hashtag.js
@@ -1,16 +1,10 @@
-import React from 'react';
-import Styles from './hashtag.module.css';
+import React from 'react'
+import Styles from './HashTag.module.css'
 
-export default function HashTag({ text = '', marginB = '' }) {
+export default function HashTag({ text = '' }) {
   return (
     <>
-      <p
-        className={`${Styles.hash_tag} ${
-          marginB === 'mb8' ? Styles.marginB : ''
-        }`}
-      >
-        {text}
-      </p>
+      <p className={Styles.hash_tag}>{text}</p>
     </>
-  );
+  )
 }
