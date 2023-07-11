daa270dc71edd2b5423fc8a77afbee9b04705026
Lily Kuo <ilwitulily0209@gmail.com>
2023-07-11 18:24:07 +0800

    font family

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,22 @@
 import '@/styles/styleguide.css';
 import '@/styles/testcom.css';
 import DefaultLayout from '@/components/layout/default-layout';
+import { ConfigProvider } from 'antd';
 
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   const getLayout =
     Component.getLayout || ((page) => <DefaultLayout>{page}</DefaultLayout>);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <ConfigProvider
+      theme={{
+        token: {
+          fontFamily: 'Noto Sans TC',
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </ConfigProvider>
+  );
 }
