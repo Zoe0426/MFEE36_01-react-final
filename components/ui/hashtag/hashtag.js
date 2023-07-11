129c6678d4db3832b98fe612bcd0e98894e5e9f9
Jill <>
2023-07-11 17:57:36 +0800

    hash

--- components/ui/hashtag/hashtag.js
+++ components/ui/hashtag/hashtag.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import Styles from './hashtag.module.css';
+import React from 'react'
+import Styles from './HashTag.module.css'
 
 export default function HashTag({ text = '' }) {
   return (
     <>
       <p className={Styles.hash_tag}>{text}</p>
     </>
-  );
+  )
 }
