c3f767a8e154c094e23a4e04181904c1ac368a69
Lilian-mac <lilian4218@gmail.com>
2023-07-12 15:36:44 +0800

    改boardNav的z-index跟刪多餘圖片

---
+++ pages/forum/blog/index 2.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import Style from './postcollection.module.css'
+import BlogBanner from '@/components/ui/blogBanner/blogBanner'
+import { Col, Row } from 'antd';
+import BlogSidebar from '@/components/ui/blogSidebar/blogSidebar';
+import BlogNav from '@/components/ui/blogNav/blogNav';
+import BlogPost from '@/components/ui/blogPost/blogPost';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faPenToSquare} from '@fortawesome/free-solid-svg-icons';
+
+export default function BlogIndex() {
+  return (
+    <div className="container-outer">
+      <div className={Style.body}>
+      <BlogBanner/>
+      <Row className={Style.antRow}>
+        <Col span={6}>
+          <BlogSidebar profile='/forum_img/kabo-p6yH8VmGqxo-unsplash.jpg' memberName='莉莉安'/>
+        </Col>
+        <Col span={16}>
+            <div className={Style.blogContent}>
+                <BlogNav blogNav='我的文章' postNum='50'/>
+                <div className={Style.postContent}>
+                    <BlogPost Date='2月9日'/>
+                    <BlogPost Date='6月9日'/>
+                    <div className={Style.editBg}>
+                        <FontAwesomeIcon icon={faPenToSquare} className={Style.editIcon}/>
+                    </div>
+                </div>
+            </div>
+        </Col>
+      </Row>
+      </div>
+      
+    </div>
+  )
+}
+
+
+
+
