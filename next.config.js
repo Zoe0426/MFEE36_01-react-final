c9f4c2942adfe0b1ec950783336b8cdda5fc25fc
Lily Kuo <ilwitulily0209@gmail.com>
2023-07-14 15:15:50 +0800

    main edit config

--- next.config.js
+++ next.config.js
@@ -7,10 +7,6 @@ const nextConfig = {
   images: {
     domains: ['via.placeholder.com', 'localhost'],
   },
-<<<<<<<<< Temporary merge branch 1
-  env: {
-    API_SERVER: 'http://localhost:3002',
-  },
   // avoid cors with proxy
   // async rewrites() {
   //   return [
