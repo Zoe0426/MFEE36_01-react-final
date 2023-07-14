b1b9327e8cd106ef96229f02b944ea9c7d649265
Lilian-mac <lilian4218@gmail.com>
2023-07-14 14:44:02 +0800

    postid頁面更新

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     domains: ['via.placeholder.com', 'localhost'],
   },
+  env:{
+    API_SERVER:'http://localhost:3002'
+  }
   // avoid cors with proxy
   // async rewrites() {
   //   return [
