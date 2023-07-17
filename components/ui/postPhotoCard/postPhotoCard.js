75ed563c305b9d0cce26ec019665d1f957e6d46c
Lilian-mac <lilian4218@gmail.com>
2023-07-17 20:53:41 +0800

    board看板filter成功、看板不同類別顏色可以變換

--- components/ui/postPhotoCard/postPhotoCard.js
+++ components/ui/postPhotoCard/postPhotoCard.js
@@ -2,6 +2,54 @@ import React from 'react'
 import Style from './postPhotoCard.module.css'
 
 export default function PostPhotoCard({img='', boardImg='', boardName='', bc='',title='', content=''}) {
+
+
+  switch (boardName){
+    case '醫療板':
+      bc='#B5DDA4';
+      console.log("board_name='醫療板':#B5DDA4");
+      break;
+    case '住宿板':
+      bc='#587D71';
+      console.log("board_name='住宿板':#587D71");
+      break;
+    case '景點板':
+      bc='#FFD1BA';
+      console.log("board_name='景點板':#FFD1BA");
+      break;
+    case '美容板':
+      bc='#A6CFD5';
+      console.log("board_name='美容板':#A6CFD5");
+      break;
+    case '狗貓聚板':
+      bc='#EFBCD5';
+      console.log("board_name='狗貓聚板':#EFBCD5");
+      break;
+    case '毛孩日記板':
+      bc='#92B4A7';
+      console.log("board_name='毛孩日記板':#92B4A7");
+      break;
+    case '學校板':
+      bc='#BE97C6';
+      console.log("board_name='學校板':#BE97C6");
+      break;
+    case '餐廳板':
+      bc='#FFE381';
+      console.log("board_name='餐廳板':#FFE381");
+      break;
+    case '好物板':
+      bc='#6F8695';
+      console.log("board_name='好物板':#6F8695");
+      break;
+    case '幼犬貓板':
+      bc='#BDDBD0';
+      console.log("board_name='幼犬貓板':#BDDBD0");
+      break;
+    case '老犬貓板':
+      bc='#F4B393';
+      console.log("board_name='老犬貓板':#F4B393");
+      break;
+  }
   return (
     <div className={Style.card}>
         <div className={Style.sticker}></div>
--- pages/forum/index.js
+++ pages/forum/index.js
@@ -4,31 +4,141 @@ import PostCard from '@/components/ui/PostCard/postCard'
 import PostBanner from '@/components/ui/postBanner/postBanner'
 import BoardNav from '@/components/ui/BoardNav/boardNav'
 import PostNav from '@/components/ui/postNav/postNav'
-import PostPhotoCard from '@/components/ui/postPhotoCard/postPhotoCard'
 import PostBottom from '@/components/ui/postBottom/postBottom'
 export default function Post() {
   const [forumData, setForumData] = useState([]);
+  const [data, setData] = useState([]);
+  // const [lateD, setlateD] = useState([]);
   const fetchData = async()=>{
     const response = await fetch ('http://localhost:3002/forum-api', {method:"GET"});
     const forumData = await response.json();
     setForumData(forumData);
+    setData(forumData); //在 fetchData 函式中，將獲取的數據存入了 forumData 狀態變數，同時也將數據存入了 data 狀態變數
     console.log('forumData', forumData);
   };
   useEffect(()=>{
     fetchData();  
   }, []);
-
-  
-
+ //醫療版
+  const doctor = ()=>{
+    const newData=forumData.filter((data)=>
+      data.board_sid===1
+    );
+    setData(newData); //將篩選後的數據存入 newData  //使用 setData 更新 data 狀態變數，將篩選後的數據存入其中
+  }
+  //住宿版
+  const home = ()=>{
+    const newData=forumData.filter((data)=>
+      data.board_sid===2
+    );
+    setData(newData);
+  }
+  //景點版
+  const site = ()=>{
+    const newData=forumData.filter((data)=>
+      data.board_sid===3
+    );
+    setData(newData);
+  }
+  //餐廳版
+  const restaurant=()=>{
+    const newData=forumData.filter((data)=>
+      data.board_sid===8
+    );
+    setData(newData);
+  }
+  //美容版
+  const salon=()=>{
+    const newData=forumData.filter((data)=>
+      data.board_sid===4
+    );
+    setData(newData);
+  }
+  //學校版
+  const school=()=>{
+    const newData=forumData.filter((data)=>
+      data.board_sid===7
+    );
+    setData(newData);
+  }
+  //狗貓聚版
+  const hang=()=>{
+    const newData=forumData.filter((data)=>
+      data.board_sid===5
+    );
+    setData(newData);
+  }
+  //幼犬貓板
+  const young=()=>{
+    const newData=forumData.filter((data)=>
+      data.board_sid===11
+    );
+    setData(newData);
+  }
+  //老犬貓板
+  const old=()=>{
+    const newData=forumData.filter((data)=>
+      data.board_sid===12
+    );
+    setData(newData);
+  }
+  //好物版
+  const product=()=>{
+    const newData=forumData.filter((data)=>
+      data.board_sid===9
+    );
+    setData(newData);
+  }
+  //毛孩日記
+  const diary=()=>{
+    const newData=forumData.filter((data)=>
+      data.board_sid===6
+    );
+    setData(newData);
+  }
   return (
     <>
       <div className="container-outer">
         <div className={Style.body}>
           <PostBanner/>
-          <BoardNav/>
+          <BoardNav 
+          doctor={()=>{
+            doctor();
+          }}
+          home={()=>{
+            home();
+          }}
+          site={()=>{
+            site();
+          }}
+          restaurant={()=>{
+            restaurant();
+          }}
+          salon={()=>{
+            salon();
+          }}
+          school={()=>{
+            school();
+          }}
+          hang={()=>{
+            hang();
+          }}
+          young={()=>{
+            young();
+          }}
+          old={()=>{
+            old();
+          }}
+          product={()=>{
+            product();
+          }}
+          diary={()=>{
+            diary()
+          }}
+          />
           <div className="container-inner">
           <PostNav postNav='熱門文章' optionCh='熱門文章' op1='最新文章'/>
-          {forumData.map((v)=>(
+          {data.map((v)=>(
            
             <PostCard key={v.post_sid}
             profile='./forum_img/victor-grabarczyk-N04FIfHhv_k-unsplash.jpg' 
@@ -39,9 +149,7 @@ export default function Post() {
             img={`http://localhost:3000/forum_img/post_img/${v.file}`} 
             likes={v.postLike} 
             comments={v.postComment} 
-            favorites={v.postFavlist}/>
-      
-            
+            favorites={v.postFavlist}/>           
   ))}         
            </div>
           <PostBottom/>
