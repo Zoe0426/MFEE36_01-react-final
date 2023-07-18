0b16c313e5df3327217ac626e21ba3401555cfac
Lily Kuo <ilwitulily0209@gmail.com>
2023-07-18 08:23:59 +0800

    add localstoragehooks

---
+++ hooks/useLocalStorageJson.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+
+export default function useLocalStorageJson(key, init = {}) {
+  const [val, setVal] = useState(init);
+
+  useEffect(() => {
+    const str = localStorage.getItem(key);
+    let data = {};
+    if (str) {
+      try {
+        data = JSON.parse(str);
+      } catch (ex) {}
+    }
+    setVal(data);
+  }, [key]);
+
+  const saveVal = (v) => {
+    localStorage.setItem(key, JSON.stringify(v));
+    setVal(v);
+  };
+
+  return [val, saveVal];
+}
