28c643e6709230f78d52a3d6961d620274e7cfb5
Lilian-mac <lilian4218@gmail.com>
2023-07-22 14:12:25 +0800

    存

--- pages/forum/blog/postcollection.js
+++ pages/forum/blog/postcollection.js
@@ -1,14 +1,24 @@
-import React from 'react'
-import Style from './index.module.css'
-import BlogBanner from '@/components/ui/blogBanner/blogBanner'
-import { Col, Row } from 'antd';
+import React, { useContext, useState, useEffect } from 'react';
+import Style from './postcollection.module.css';
+import BlogBanner from '@/components/ui/blogBanner/blogBanner';
+import { Col, Row, Pagination} from 'antd';
 import BlogSidebar from '@/components/ui/blogSidebar/blogSidebar';
-import PostNav from '@/components/ui/postNav/postNav';
-import BlogPost from '@/components/ui/blogPost/blogPost';
+import BlogNav from '@/components/ui/blogNav/blogNav';
+import PostCard from '@/components/ui/PostCard/postCard';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faPenToSquare} from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import AuthContext from '@/context/AuthContext';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-export default function BlogIndex() {
+export default function PostCollection() {
+  const [data, setData] = useState({
+    totalRows: 0,
+    perPage: 15,
+    totalPages: 0,
+    page: 1,
+    rows: [], // 使用 rows 屬性來存放資料陣列
+  });
   return (
     <div className="container-outer">
       <div className={Style.body}>
