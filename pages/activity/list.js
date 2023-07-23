c7b063c8903ac1961412b61df2ff853aef532d9f
Jennyname <whoiskenny0408@gmail.com>
2023-07-23 21:44:36 +0800

    32nd push - auth小小的修改

--- pages/activity/list.js
+++ pages/activity/list.js
@@ -44,8 +44,6 @@ export default function ActivityMain() {
 
   //會員是否登入
   useEffect(() => {
-    let auth = {};
-    const authStr = localStorage.getItem('petauth');
     
     if (auth.token) {
       const fetchData = async () => {
