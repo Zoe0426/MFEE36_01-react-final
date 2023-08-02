0c8e79e9af72bd78d713698fff02961731ffa863
Lily Kuo <ilwitulily0209@gmail.com>
2023-08-02 15:49:53 +0800

    update coupon style

--- components/ui/cart/cartcouponinfo.js
+++ components/ui/cart/cartcouponinfo.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import style from './cartcouponinfo.module.css';
-import { Radio } from 'antd';
 
 export default function CartCouponInfo({
-  coupon_send_sid = '',
   exp_date = '',
   name = '',
   price = 0,
-  selected = false,
-  // radioBtn = true,
 }) {
   return (
     <div className={style.info}>
       <div className={style.details}>
-        <p className={style.name}>{name}</p>
-        <p className={style.expDate}>
-          使用期限&nbsp;&nbsp;
-          <span className={style.expDate}>{exp_date}</span>
-        </p>
+        <div className={style.namebox}>
+          <p className={style.name}>{name}</p>
+          <p className={style.price}>折抵${price}</p>
+        </div>
+
+        <p className={style.expDate}>有效期限：{exp_date}</p>
       </div>
-      <div>
-        <span className={style.postPrice}>-${price}&nbsp;&nbsp;</span>
-      </div>
+      <img src="/home-images/h-logo.png" alt="" className={style.img} />
     </div>
   );
 }
