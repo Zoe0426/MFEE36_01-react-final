6d85dd2be4bf19df6f76bc0b40005076fbd02d94
Zoe0426 <90545407+Zoe0426@users.noreply.github.com>
2023-08-06 21:18:46 +0800

    每日簽到更新

--- pages/member/petGame.js
+++ pages/member/petGame.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react';
-const OFFSET_X = 300;
-const OFFSET_Y = 200;
-
+import SecondNavbar from '@/components/layout/SecondNavbar';
+import Draggable from 'react-draggable';
+const OFFSET_X = 206;
+const OFFSET_Y = 150;
+const waitTimeMin = 500;
+const waitTimeMax = 3000;
 export default function PetGame() {
   const lastImageSwitchTime = useRef(null);
   const [moveImageIndex, setMoveImageIndex] = useState(0);
@@ -15,9 +18,79 @@ export default function PetGame() {
   const [moveCount, setMoveCount] = useState(0);
   const [isMoving, setIsMoving] = useState(false);
   const [isCatVisible, setIsCatVisible] = useState(false);
-  // 將 ANIMATION_DURATION 搬到 state
   const [animationDuration, setAnimationDuration] = useState(50);
+  const [isDragging, setIsDragging] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [dragPosition, setDragPosition] = useState({ x: 0, y: 0 });
+  const [count, setCount] = useState(0);
+  const [catImage, setCatImage] = useState('/pet_game/cat_idle.png');
+  const [catTransform, setCatTransform] = useState('scaleX(1)');
+  let [transform, setTransform] = useState('scaleX(1)');
+  const [foods, setFoods] = useState([]);
+  // 在拖曳開始時設定 isDragging 為 true 和 isPaused 為 true
+  const onStartDrag = (e) => {
+    e.preventDefault();
+    setIsDragging(true);
+    setIsPaused(true);
+  };
 
+  // 在拖曳結束時設定 isDragging 為 false 和 isPaused 為 false
+  const onStopDrag = (e) => {
+    e.preventDefault();
+    setIsDragging(false);
+    setIsPaused(false);
+    setMousePosition({ x: e.clientX, y: e.clientY });
+    setDragPosition({ x: 0, y: 0 });
+
+    const finalPosition = { x: e.clientX, y: e.clientY };
+
+    const rect = background.current.getBoundingClientRect();
+    const offwidth = rect.width * 0.3;
+    const minX = rect.left + offwidth;
+    const maxX = rect.right - offwidth;
+    const minY = rect.top + OFFSET_Y;
+    const maxY = rect.bottom - OFFSET_Y;
+
+    if (
+      // finalPosition.x >= minX &&
+      // finalPosition.x <= maxX &&
+      finalPosition.y <= maxY &&
+      finalPosition.y >= minY
+    ) {
+      setFoods((prevFoods) => [...prevFoods, finalPosition]);
+    }
+
+    setMousePosition({ x: e.clientX, y: e.clientY });
+    setDragPosition({ x: 0, y: 0 });
+  };
+
+  // 強制刷新组件的函数
+  const forceUpdate = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  // 在拖曳時更新 dragPosition
+  const onDrag = (e, data) => {
+    setMousePosition({ x: e.clientX, y: e.clientY });
+    setDragPosition({ x: data.x, y: data.y });
+  };
+  useEffect(() => {
+    if (isDragging) {
+      // 如果正在拖動，則強制更新貓的組件
+      forceUpdate();
+    }
+  }, [dragPosition]);
+
+  useEffect(() => {
+    if (isDragging && !isMoving) {
+      setTransform(mousePosition.x < position.x ? 'scaleX(-1)' : 'scaleX(1)');
+    } else if (isMoving) {
+      setTransform(direction === 'left' ? 'scaleX(-1)' : 'scaleX(1)');
+    }
+  }, [mousePosition, direction, isMoving]);
+
+  //循環自主移動
   useEffect(() => {
     let restTimeout = null;
     if (!isAnimating && !isResting && background.current) {
@@ -26,17 +99,31 @@ export default function PetGame() {
 
       const minX = rect.left + offwidth;
       const maxX = rect.right - offwidth;
-      const minY = rect.top + OFFSET_X;
+      const minY = rect.top + OFFSET_Y;
       const maxY = rect.bottom - OFFSET_Y;
+
+      let closestFood = null;
+      let minDistance = Infinity;
+      foods.forEach((food, index) => {
+        const distance = Math.sqrt(
+          Math.pow(food.x - position.x, 2) + Math.pow(food.y - position.y, 2)
+        );
+        if (distance < minDistance) {
+          closestFood = { ...food, index };
+          minDistance = distance;
+        }
+      });
 
       let targetX;
       let targetY;
+
       if (moveCount === 0) {
-        // 如果 moveCount 為 1，則將目標設為中心
         targetX = rect.width / 2 - 100;
         targetY = rect.height / 2;
+      } else if (closestFood) {
+        targetX = closestFood.x;
+        targetY = closestFood.y;
       } else {
-        // 如果不是，就隨機位置
         targetX = Math.random() * (maxX - minX) + minX;
         targetY = Math.random() * (maxY - minY) + minY;
       }
@@ -62,7 +149,6 @@ export default function PetGame() {
           lastImageSwitchTime.current = animationTime;
         }
 
-        // Calculate new position
         const newPosition = {
           x: position.x + (targetPosition.x - position.x) * progress,
           y: position.y + (targetPosition.y - position.y) * progress,
@@ -72,11 +158,9 @@ export default function PetGame() {
         } else {
           setDirection('right');
         }
-        // Update position state
         setPosition(newPosition);
 
-        // Continue animation if not finished
-        if (progress < 1) {
+        if (progress < 1 && !isPaused) {
           requestAnimationFrame(animate);
         } else {
           setIsAnimating(false);
@@ -84,6 +168,14 @@ export default function PetGame() {
           lastImageSwitchTime.current = null;
           setIsMoving(false);
           setIsResting(true);
+
+          // Step 2: Remove the food at position.
+          if (closestFood && progress >= 1) {
+            setFoods((prevFoods) =>
+              prevFoods.filter((_, index) => index !== closestFood.index)
+            );
+          }
+
           setMoveCount((prevCount) => prevCount + 1);
           if (moveCount === 1) {
             setIsCatVisible(true);
@@ -94,7 +186,7 @@ export default function PetGame() {
           } else {
             restTimeout = setTimeout(
               () => setIsResting(false),
-              Math.random() * 2000 + 1000
+              Math.random() * waitTimeMax + waitTimeMin
             );
           }
         }
@@ -104,33 +196,126 @@ export default function PetGame() {
     }
 
     return () => clearTimeout(restTimeout);
-  }, [isAnimating, isResting]);
+  }, [isAnimating, isResting, position, foods]);
+
+  useEffect(() => {
+    const rect = background.current.getBoundingClientRect();
+    const offwidth = rect.width;
+
+    const minX = rect.left + offwidth;
+    const maxX = rect.right - offwidth;
+    const minY = rect.top + OFFSET_Y;
+    const maxY = rect.bottom - OFFSET_Y;
+
+    let closestFood = null;
+    let minDistance = Infinity;
+    foods.forEach((food, index) => {
+      const distance = Math.sqrt(
+        Math.pow(food.x - position.x, 2) + Math.pow(food.y - position.y, 2)
+      );
+      if (distance < minDistance) {
+        closestFood = { ...food, index };
+        minDistance = distance;
+      }
+    });
+
+    let targetX;
+    let targetY;
+
+    if (moveCount === 0) {
+      targetX = rect.width / 2 - 100;
+      targetY = rect.height / 2;
+    } else if (closestFood) {
+      targetX = closestFood.x - OFFSET_X * 0.5;
+      targetY = closestFood.y - OFFSET_Y;
+    } else {
+      targetX = Math.random() * (maxX - minX) + minX;
+      targetY = Math.random() * (maxY - minY) + minY;
+    }
+
+    setTargetPosition((prevPos) => ({ ...prevPos, x: targetX, y: targetY }));
+  }, [isAnimating, isResting, position, foods]);
 
   return (
     <div className="background" ref={background}>
+      <SecondNavbar />
       {isCatVisible && (
         <Cat
           position={position}
           direction={direction}
           isMoving={isMoving}
+          isDragging={isDragging}
           moveImageIndex={moveImageIndex}
+          mousePosition={dragPosition}
+          transform={transform}
         />
       )}
+      <Draggable
+        position={{ x: 0, y: 0 }}
+        onStart={onStartDrag}
+        onStop={onStopDrag}
+        onDrag={onDrag}
+      >
+        <img
+          src="/pet_game/Food_01.png"ﬁ
+          style={{
+            position: 'absolute',
+            bottom: isDragging ? '170px' : '170px',
+            right: isDragging ? '130px' : '150px',
+            opacity: isDragging ? 1 : 1, // 在拖曳時，將 opacity 設為 0.5
+          }}
+          alt="Food"
+        />
+      </Draggable>
+      {/* Show a semi-transparent clone while dragging */}
+      {
+        <img
+          src="/pet_game/Food_01.png"
+          style={{
+            position: 'fixed',
+            pointerEvents: 'none',
+            bottom: 170,
+            right: 150,
+            opacity: 0.5,
+          }}
+          alt="Dragging food"
+        />
+      }
+      {foods.map((foodPosition, index) => (
+        <img
+          key={index}
+          src="/pet_game/Food_01.png"
+          style={{
+            position: 'absolute',
+            left: foodPosition.x,
+            top: foodPosition.y,
+            zIndex: Math.floor(foodPosition.y),
+          }}
+          alt="Food"
+        />
+      ))}
     </div>
   );
 }
 
-const Cat = ({ position, direction, isMoving, moveImageIndex }) => {
+const Cat = ({ position, isMoving, isDragging, moveImageIndex, transform }) => {
+  let imageSource;
+
+  if (isDragging && !isMoving) {
+    imageSource = '/pet_game/cat_idle_food.png';
+  } else {
+    imageSource = isMoving
+      ? `/pet_game/cat_move0${moveImageIndex + 1}.png`
+      : '/pet_game/cat_idle.png';
+  }
+
   const petStyle = {
     position: 'absolute',
     left: position.x,
     top: position.y,
-    // 如果direction是left，則將scaleX設置為-1
-    transform: direction === 'left' ? 'scaleX(-1)' : 'scaleX(1)',
+    transform: transform,
+    zIndex: Math.floor(position.y + 150),
   };
-  const imageSource = isMoving
-    ? `/pet_game/cat_move0${moveImageIndex + 1}.png`
-    : '/pet_game/cat_idle.png';
 
   return <img src={imageSource} style={petStyle} alt="pet" />;
 };
