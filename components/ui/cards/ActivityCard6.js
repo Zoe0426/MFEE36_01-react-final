f6e8d2e910abf58f417ff93e166350b4e5dd1acf
Jennyname <whoiskenny0408@gmail.com>
2023-08-10 15:49:06 +0800

    86h push - 投票 努力中

--- components/ui/cards/ActivityCard6.js
+++ components/ui/cards/ActivityCard6.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faHand,
@@ -11,14 +11,34 @@ import IconMainBtn from '@/components/ui/buttons/IconMainBtn';
 const ActivityCard6 = ({
   member_sid = '',
   title = '',
-  count = 0,
+  initialCount = 0, // 使用 initialCount 作為初始 count
   city = '',
   area = '',
   profile = null,
-  content= '',
-  other_message= null,
+  content = '',
+  other_message = null,
+  hasVoted = false,
+  setHasVoted,
   handleVote,
+  setVotedMembers,
 }) => {
+  const [count, setCount] = useState(initialCount); // 使用 count 狀態
+
+  const [isVoted, setIsVoted] = useState(hasVoted);
+
+  const handleVoteClick = async () => {
+    // 處理投票邏輯
+    await handleVote();
+
+    // 更新投票狀態和 count
+    setHasVoted(true); // 更新 hasVoted 狀態
+    setCount(count + 1);
+    setIsVoted(true);
+
+    // 更新已投票的會員陣列
+    setVotedMembers((prevVotedMembers) => [...prevVotedMembers, member_sid]);
+  };
+
   return (
     <div className={styles.card}>
       <div className={styles.right_section}>
@@ -33,10 +53,10 @@ const ActivityCard6 = ({
             <FontAwesomeIcon className={styles.image} icon={faUser} />
           )}
         </div>
-        <IconMainBtn 
-        icon={faHand} 
-        text="投我一票" 
-        clickHandler={handleVote}
+        <IconMainBtn
+          icon={faHand}
+          text={isVoted ? '已投票' : '投我一票'}
+          clickHandler={isVoted ? undefined : handleVoteClick}
         />
       </div>
 
