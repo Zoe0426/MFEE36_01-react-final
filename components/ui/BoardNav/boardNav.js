e4835b1aed7361fe6917a767126ac24b126a0022
Lilian-mac <lilian4218@gmail.com>
2023-08-10 21:17:56 +0800

    看板點選成功換色

--- components/ui/BoardNav/boardNav.js
+++ components/ui/BoardNav/boardNav.js
@@ -1,14 +1,12 @@
 import React, { useState } from 'react'
 import Style from './boardNav.module.css'
-import SubBtn from '../buttons/subBtn'
-import PostAuthorBTN from '../postAuthorBtn/postAuthorBtn'
-import BlogDecoration from '../blogBanner/blogDecoration'
+import SubBtnBoard from '../buttons/subBtnBoard'
 import BGUpperDecoration from '../decoration/bg-upper-decoration'
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faChevronRight,faChevronLeft} from '@fortawesome/free-solid-svg-icons';
 
-export default function BoardNav({doctor,home,site,restaurant,salon,school,hang,young,old,product,diary, img='',text='',PostAuthorBTNHandler=()=>{}}) {
+export default function BoardNav({changeBoardSid, board_sid=1, img='',text='',PostAuthorBTNHandler=()=>{}}) {
 
   const [move, setMove] = useState(0);
 
@@ -47,38 +45,60 @@ export default function BoardNav({doctor,home,site,restaurant,salon,school,hang,
 
       <div className={Style.boardHidden}>
         <div className={Style.boardRange} style={{ left: move === 1 ? '-650px' : '0' }}>
-            <div className={Style.board} onClick={doctor}>
-              <SubBtn img = '/forum_img/board_img/寵物醫療版.png' text ='醫療板'/>
+            <div className={Style.board}>
+            <SubBtnBoard img="/forum_img/board_img/寵物醫療版.png" text="醫療板" board_sid={board_sid} isFill={board_sid===1?true:false} subBtnHandler={()=>{
+              changeBoardSid(1)
+            }}/>
             </div>
-            <div className={Style.board} onClick={home}>
-              <SubBtn img = '/forum_img/board_img/住宿版.png' text ='住宿板'/>
+            <div className={Style.board}>
+            <SubBtnBoard img="/forum_img/board_img/住宿版.png" text="住宿板" board_sid={board_sid} isFill={board_sid===2?true:false} subBtnHandler={()=>{
+              changeBoardSid(2)
+            }}/>
             </div>
-            <div className={Style.board} onClick={site}>
-              <SubBtn img = '/forum_img/board_img/景點版.png' text ='景點板'/>
+            <div className={Style.board}>
+            <SubBtnBoard img="/forum_img/board_img/景點版.png" text="景點板" board_sid={board_sid} isFill={board_sid===3?true:false} subBtnHandler={()=>{
+              changeBoardSid(3)
+            }}/>
             </div>
-            <div className={Style.board} onClick={restaurant}>
-              <SubBtn img = '/forum_img/board_img/餐廳版.png' text ='餐廳板'/>
+            <div className={Style.board}>
+            <SubBtnBoard img="/forum_img/board_img/餐廳版.png" text="餐廳板" board_sid={board_sid} isFill={board_sid===8?true:false} subBtnHandler={()=>{
+              changeBoardSid(8)
+            }}/>
             </div>
-            <div className={Style.board} onClick={salon}>
-              <SubBtn img = '/forum_img/board_img/美容版.png' text ='美容板'/>
+            <div className={Style.board}>
+            <SubBtnBoard img="/forum_img/board_img/美容版.png" text="美容板" board_sid={board_sid} isFill={board_sid===4?true:false} subBtnHandler={()=>{
+              changeBoardSid(4)
+            }}/>
             </div>
-            <div className={Style.board} onClick={school}>
-              <SubBtn img = '/forum_img/board_img/學校版.png' text ='學校板'/>
+            <div className={Style.board}>
+            <SubBtnBoard img="/forum_img/board_img/學校版.png" text="學校板" board_sid={board_sid} isFill={board_sid===7?true:false} subBtnHandler={()=>{
+              changeBoardSid(7)
+            }}/>
             </div>
-            <div className={Style.board} onClick={hang}>
-              <SubBtn img = '/forum_img/board_img/狗貓聚.png' text ='狗貓聚板'/>
+            <div className={Style.board}>
+            <SubBtnBoard img="/forum_img/board_img/狗貓聚.png" text="狗貓聚板" board_sid={board_sid} isFill={board_sid===5?true:false} subBtnHandler={()=>{
+              changeBoardSid(5)
+            }}/>
             </div>
-            <div className={Style.board} onClick={young}>
-              <SubBtn img = '/forum_img/board_img/幼犬貓板.png' text ='幼犬貓板'/>
+            <div className={Style.board}>
+            <SubBtnBoard img="/forum_img/board_img/幼犬貓板.png" text="幼犬貓板" board_sid={board_sid} isFill={board_sid===11?true:false} subBtnHandler={()=>{
+              changeBoardSid(11)
+            }}/>
             </div>
-            <div className={Style.board} onClick={old}>
-              <SubBtn img = '/forum_img/board_img/老狗貓版.png' text ='老犬貓板'/>
+            <div className={Style.board}>
+            <SubBtnBoard img="/forum_img/board_img/老狗貓版.png" text="老犬貓板" board_sid={board_sid} isFill={board_sid===12?true:false} subBtnHandler={()=>{
+              changeBoardSid(12)
+            }}/>
             </div>
-            <div className={Style.board} onClick={product}>
-              <SubBtn img = '/forum_img/board_img/好物分享版.png' text ='好物板'/>
+            <div className={Style.board}>
+            <SubBtnBoard img="/forum_img/board_img/好物分享版.png" text="好物板" board_sid={board_sid} isFill={board_sid===9?true:false} subBtnHandler={()=>{
+              changeBoardSid(9)
+            }}/>
             </div>
-            <div className={Style.board} onClick={diary}>
-              <SubBtn img = '/forum_img/board_img/毛孩日記版.png' text ='毛孩日記板'/>
+            <div className={Style.board}>
+            <SubBtnBoard img="/forum_img/board_img/毛孩日記版.png" text="毛孩日記板" board_sid={board_sid} isFill={board_sid===6?true:false} subBtnHandler={()=>{
+              changeBoardSid(6)
+            }}/>
             </div>
             </div>
           </div>
