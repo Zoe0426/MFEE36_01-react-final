01be68037da116ea5c3bcfc79a61a70826a07bf3
Shuyi <shuyi041@gmail.com>
2023-08-11 11:14:17 +0800

    s_finish emoji

--- components/ui/shop/chatroom.js
+++ components/ui/shop/chatroom.js
@@ -29,127 +29,132 @@ export default function Chatroom({
     chatroomRef.current.scrollTop = chatroomRef.current.scrollHeight;
   }, [chatroomDatas]);
   return (
-    <div className={styles.chatroom_box}>
-      <div className={styles.chatroom_head}>
-        <h6>狗with貓 線上客服</h6>
-        <FontAwesomeIcon
-          style={{ maxWidth: '16px', maxHeight: '16px' }}
-          icon={faXmark}
-          className={styles.btn_clear_text}
-          onClick={closeHandler}
-        />
-      </div>
-      <div className={styles.chatroom_textarea} ref={chatroomRef}>
-        <ul>
-          {chatroomDatas.map((v, i) => {
-            return (
-              <li
-                key={i}
-                className={
-                  v.message.message.includes('已離開聊天室')
-                    ? styles.leave_text
-                    : v.message.sender === username
-                    ? styles.custom_text
-                    : styles.service_text
-                }
-              >
-                {v.message.message.includes('已離開聊天室')
-                  ? ''
-                  : v.sender !== username && (
-                      <div
-                        className={
-                          i !== 0 && chatroomDatas[i - 1].sender !== username
-                            ? `${styles.head_img_no_show} ${styles.order0}`
-                            : styles.head_img
-                        }
-                      >
-                        {v.message.img ? (
-                          <img
-                            src={`${process.env.API_SERVER}/img/${v.message?.img}`}
-                            alt={v.sender}
-                          />
-                        ) : (
-                          <FontAwesomeIcon
-                            icon={faUser}
-                            className={styles.default_head}
-                          />
-                        )}
-                      </div>
-                    )}
-                {!v.message.message.includes('已離開聊天室') && (
+    <>
+      <div className={styles.chatroom_box}>
+        <div className={styles.chatroom_head}>
+          <h6>狗with貓 線上客服</h6>
+          <FontAwesomeIcon
+            style={{ maxWidth: '16px', maxHeight: '16px' }}
+            icon={faXmark}
+            className={styles.btn_clear_text}
+            onClick={closeHandler}
+          />
+        </div>
+        <div className={styles.chatroom_textarea} ref={chatroomRef}>
+          <ul>
+            {chatroomDatas.map((v, i) => {
+              return (
+                <li
+                  key={i}
+                  className={
+                    v.message.message.includes('已離開聊天室')
+                      ? styles.leave_text
+                      : v.message.sender === username
+                      ? styles.custom_text
+                      : styles.service_text
+                  }
+                >
+                  {v.message.message.includes('已離開聊天室')
+                    ? ''
+                    : v.sender !== username && (
+                        <div
+                          className={
+                            i !== 0 && chatroomDatas[i - 1].sender !== username
+                              ? `${styles.head_img_no_show} ${styles.order0}`
+                              : styles.head_img
+                          }
+                        >
+                          {v.message.img ? (
+                            <img
+                              src={`${process.env.API_SERVER}/img/${v.message?.img}`}
+                              alt={v.sender}
+                            />
+                          ) : (
+                            <FontAwesomeIcon
+                              icon={faUser}
+                              className={styles.default_head}
+                            />
+                          )}
+                        </div>
+                      )}
+                  {!v.message.message.includes('已離開聊天室') && (
+                    <span
+                      className={
+                        v.sender !== username
+                          ? `${styles.time} ${styles.order3}`
+                          : `${styles.time} ${styles.order0}`
+                      }
+                    >
+                      {v.message.time}
+                    </span>
+                  )}
                   <span
                     className={
                       v.sender !== username
-                        ? `${styles.time} ${styles.order3}`
-                        : `${styles.time} ${styles.order0}`
+                        ? styles.text
+                        : `${styles.text} ${styles.bgc}`
                     }
                   >
-                    {v.message.time}
+                    {v.message.message}
                   </span>
-                )}
-                <span
-                  className={
-                    v.sender !== username
-                      ? styles.text
-                      : `${styles.text} ${styles.bgc}`
-                  }
-                >
-                  {v.message.message}
-                </span>
-                {v.sender === username && (
-                  <div
-                    className={
-                      i !== 0 && chatroomDatas[i - 1]?.sender === username
-                        ? styles.head_img_no_show
-                        : styles.head_img
-                    }
-                  >
-                    {v.message.img ? (
-                      <img
-                        src={`${process.env.API_SERVER}/img/${v.message?.img}`}
-                        alt={v.sender}
-                      />
-                    ) : (
-                      <FontAwesomeIcon
-                        icon={faUser}
-                        className={styles.default_head}
-                      />
-                    )}
-                  </div>
-                )}
-              </li>
-            );
-          })}
-        </ul>
-        {showEmojiPicker && (
-          <div className={styles.emoji_box}>
-            <Picker
-              data={data}
-              searchPosition="none"
-              maxFrequentRows={0}
-              previewPosition="none"
-              onEmojiSelect={pickHandler}
-            />
-          </div>
-        )}
-      </div>
+                  {v.sender === username && (
+                    <div
+                      className={
+                        i !== 0 && chatroomDatas[i - 1]?.sender === username
+                          ? styles.head_img_no_show
+                          : styles.head_img
+                      }
+                    >
+                      {v.message.img ? (
+                        <img
+                          src={`${process.env.API_SERVER}/img/${v.message?.img}`}
+                          alt={v.sender}
+                        />
+                      ) : (
+                        <FontAwesomeIcon
+                          icon={faUser}
+                          className={styles.default_head}
+                        />
+                      )}
+                    </div>
+                  )}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
 
-      <div className={styles.sending_box}>
-        <FontAwesomeIcon
-          icon={faFaceSmile}
-          onClick={toggleDisplayForEmoji}
-          className={styles.emoji_smile}
-        />
-        <textarea
-          name="chatroomText"
-          value={inputText}
-          onChange={changeHandler}
-          onKeyDown={keyDownHandler}
-          placeholder="請輸入訊息"
-        />
+        <div className={styles.sending_box}>
+          <FontAwesomeIcon
+            icon={faFaceSmile}
+            onClick={toggleDisplayForEmoji}
+            className={styles.emoji_smile}
+          />
+          <textarea
+            name="chatroomText"
+            value={inputText}
+            onChange={changeHandler}
+            onKeyDown={keyDownHandler}
+            placeholder="請輸入訊息"
+          />
 
-        <button onClick={clickHandler}>傳送</button>
-      </div>
-    </div>
+          <button onClick={clickHandler}>傳送</button>
+        </div>
+      </div>{' '}
+      {showEmojiPicker && (
+        <div className={styles.emoji_box}>
+          <Picker
+            data={data}
+            searchPosition="none"
+            maxFrequentRows={0}
+            previewPosition="none"
+            onEmojiSelect={pickHandler}
+            theme="light"
+            navPosition="none"
+            perLine={10}
+          />
+        </div>
+      )}
+    </>
   );
 }
