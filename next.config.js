6f64e6d958d4615ffb3f944decdf3e8b26484552
Lily Kuo <ilwitulily0209@gmail.com>
2023-08-29 20:07:29 +0800

    config

--- next.config.js
+++ next.config.js
@@ -3,22 +3,22 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     //平常開發使用本端
-    API_SERVER: 'http://localhost:3002',
-    WEB: 'http://localhost:3000',
+    // API_SERVER: 'http://localhost:3002',
+    // WEB: 'http://localhost:3000',
     //發表時使用
     // SHUYI
     // API_SERVER: 'http://192.168.24.183:3002',
     // WEB: 'http://192.168.24.183:3000',
     // WS: 'ws://192.168.24.183:3002',
     // SHUYI HOME
-    // API_SERVER: 'http://192.168.1.107:3002',
-    // WEB: 'http://192.168.1.107:3000',
-    // WS: 'ws://192.168.1.107:3002',
+    API_SERVER: 'http://192.186.0.187:3002',
+    WEB: 'http://192.186.0.187:3000',
+    WS: 'ws://192.168.0.187:3002',
     //JILL
     // API_SERVER: 'http://192.168.24.19:3002',
     // WEB: 'http://192.168.24.19:3000',
     //LILY
-    // API_SERVER: 'http://192.168.24.68:3002',
+    // API_SERVER: 'http://114.32.150.166:3002',
     // WEB: 'http://192.168.24.68:3000',
     //發表時要再後端.env檔使用
     // EMAIL_USER= gowithmeispan@gmail.com
